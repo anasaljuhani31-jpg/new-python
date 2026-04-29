ffd594fdf979a1877478a5f78d48371ab8c44f5e
anasaljuhani31-jpg <anasaljuhani31@gmail.com>
2026-04-30 01:06:15 +0300

    lab 5 finish

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse # تأكد من وجود هذا السطر
 
-# Create your views here.
+def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
+def links_page(request):
+    return render(request, 'bookmodule/links.html')
+def formatting_page(request):
+    return render(request, 'bookmodule/formatting.html')
+def listing_page(request):
+    return render(request, 'bookmodule/listing.html')
+def tables_page(request):
+    return render(request, 'bookmodule/tables.html')
